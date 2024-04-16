2510a462805a117b10df2681c1964a2bfc5a8842
fab <prateeshop@gmail.com>
2024-04-16 13:25:56 +0530

    batmannnn

--- main.py
+++ main.py
@@ -20,11 +20,57 @@ def Dialogue(sentence, interval, clear):
     else:
         pass
 
+def Choice(choice1, choice2):
+    print("1. " + choice1)
+    print("2. " + choice2)
+
+def Location(place):
+    places = {1: "Coffee Shop",
+              2: "Downtown Subway",
+              3: "Convinience Store",
+              4: "Warehouse",
+              5: "Underground",
+              6: "Laboratory",
+              7: "Diable Room",
+              8: "Future",
+              9: "Past",
+              10: "Before The Wakening",
+              11: "Hyperspace",
+              12: "Hivemind",
+              13: "The Codebase",
+              14: "The Prism"}
+
+    List = list(places.values())
+    if place in List:
+        Clear()
+        print(place)
+
+    ans = input("> ")
+    if ans == 1:
+        return 1
+    elif ans == 2:
+        return 2
+    else:
+        return 2
+
 def Act1():
     Clear()
-    print("ACT 1")
+    print("░▒▓ ACT I ▓▒░")
     time.sleep(5)
     Clear()
     Dialogue("You have come to a famous coffee shop (surprising)", 3, 0)
     Dialogue("You sit on the table nearest to you, it's begun raining outside...", 2, 0)
-    Dialogue("{characters[0]} What would you like sir?", 2, 0)
+    Dialogue("Waiter: What would you like sir?", 2, 0)
+    print("[Menu: Latte, Macchiato, Mocha, Espresso, Frappe]   (write first letter capital damnit)")
+    order = input("> ")
+    if order not in ["Latte", "Macchiato", "Mocha", "Espresso"]:
+        print("{characters[0]} Thats not in the menu sir")
+        input("> ")
+    else:
+        print("One ", order, " coming right up!")
+        time.sleep(2)
+
+    Dialogue("A man suited in black sits next to you, his face is covered by a strange mask...", 2, 1)
+    Dialogue("He looks ouside the window and then turns towards you \nShady Figure: Good weather innit?", 2, 0)
+    Location("")
+Act1()
